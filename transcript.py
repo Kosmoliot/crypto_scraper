d71c7979a0d6341a721b279a6b036d1ddf300c41
Miguel <michail.pliuscik@gmail.com>
2024-03-25 16:37:51 +0000

    hid youtube API key

--- transcript.py
+++ transcript.py
@@ -1,5 +1,7 @@
+import os
 from youtube_transcript_api import YouTubeTranscriptApi
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
 
 
 def transcript(video_id):
@@ -11,24 +13,24 @@ def transcript(video_id):
     
 
 
-# # Define API key and channel ID
+# Define API key and channel ID
+load_dotenv()
+API_KEY = os.environ['YOUTUBE_API_KEY']
+CHANNEL_ID = "UCHop-jpf-huVT1IYw79ymPw"
 
-# API_KEY = "AIzaSyDXIB6xTdYB996unF3cq8rwFb8wGh63WjE"
-# CHANNEL_ID = "UCHop-jpf-huVT1IYw79ymPw"
+# Define the YouTube API service
+youtube = build("youtube", "v3", developerKey=API_KEY)
 
-# # Define the YouTube API service
-# youtube = build("youtube", "v3", developerKey=API_KEY)
+# Retrieve videos from the channel
+request = youtube.search().list(
+    part="snippet",
+    channelId=CHANNEL_ID,
+    maxResults=50  # Adjust the number of results as needed
+)
+response = request.execute()
 
-# # Retrieve videos from the channel
-# request = youtube.search().list(
-#     part="snippet",
-#     channelId=CHANNEL_ID,
-#     maxResults=50  # Adjust the number of results as needed
-# )
-# response = request.execute()
-
-# # Extract video IDs from the response
-# video_ids = [item["id"]["videoId"] for item in response["items"] if item["id"]["kind"] == "youtube#video"]
+# Extract video IDs from the response
+video_ids = [item["id"]["videoId"] for item in response["items"] if item["id"]["kind"] == "youtube#video"]
 
 print(transcript('qKL0EbFms0g'))
 
