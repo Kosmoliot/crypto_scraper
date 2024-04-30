3352a2fb162ef56e831ccde306de718d09d071e6
Miguel <michail.pliuscik@gmail.com>
2024-04-30 17:25:57 +0100

    added date parse function, cleaned the code

--- transcript.py
+++ transcript.py
@@ -1,76 +1,71 @@
-import os, json
+import os
+import json
+from datetime import datetime
 from youtube_transcript_api import YouTubeTranscriptApi
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 from openai import OpenAI
-from datetime import datetime
 
 load_dotenv()
 
-# Define API key and channel ID
 API_KEY = os.getenv('YOUTUBE_API_KEY')
 if not API_KEY:
     raise ValueError("YOUTUBE_API_KEY environment variable is not set.")
 
 CHANNEL_ID = "UCHop-jpf-huVT1IYw79ymPw"
 
-class ChicoVideo():
+
+class ChicoVideo:
     """Class to store video parameters."""
-    def __init__(self, video_id, video_date, video_title, video_coins) -> None:
+    def __init__(self, video_id, video_date, video_title, video_coins):
         self.video_id = video_id
         self.video_date = video_date
         self.video_title = video_title
         self.video_coins = video_coins
 
+
 def get_transcript(video_id):
     """Retrieve transcript for a given video ID."""
     try:
         transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-
         video_transcript = ' '.join(item['text'] for item in transcript_list)
         return video_transcript
     except Exception as e:
         print(f"Failed to retrieve transcript for video {video_id}: {e}")
         return None
 
-def formatted_date(given_date):
-    """Formating date to a necessary format"""
-    date_format = given_date.split(',')
-    return datetime(int(date_format[0]), int(date_format[1]), int(date_format[2]))
+
+def parse_date(date_str):
+    """Parse date string to datetime object."""
+    return datetime.strptime(date_str, "%Y,%m,%d")
 
 
 def fetch_video_data(start_date, end_date):
-    """Retrieve video IDs, date, title, and coins list."""
+    """Retrieve video data from YouTube API."""
     try:
-        cache_dir = "cache_data"
-        os.makedirs(cache_dir, exist_ok=True)
-        cache_filename = os.path.join(cache_dir, f"cache_data_{start_date}_{end_date}.json")
+        start_date_str = parse_date(start_date).strftime('%Y-%m-%dT%H:%M:%SZ')
+        end_date_str = parse_date(end_date).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        cache_filename = f"cache_data/cache_data_{start_date}_{end_date}.json"
 
         if os.path.exists(cache_filename):
             with open(cache_filename, 'r') as file:
                 return json.load(file)
 
-
-        # Define the YouTube API service. Achieving resource cleanup by using "with" statement
         with build("youtube", "v3", developerKey=API_KEY) as youtube:
-            
-            # Define the time period
-            start_date_str = formatted_date(start_date).strftime('%Y-%m-%dT%H:%M:%SZ')
-            end_date_str = formatted_date(end_date).strftime('%Y-%m-%dT%H:%M:%SZ')
-        
-            videos =[]
+            videos = []
+
             request = youtube.search().list(
                 part="snippet",
                 channelId=CHANNEL_ID,
                 publishedAfter=start_date_str,
                 publishedBefore=end_date_str,
-                maxResults=1000,  # Adjust the number of results as needed
-                type="video",   # Necessary for using videoDuration parameter
-                videoDuration="medium" , # Video length from 4 to 20 minutes
+                maxResults=1000,
+                type="video",
+                videoDuration="medium",
             )
             response = request.execute()
-            
-            # Extract video IDs, date, title, and coim list fro, the response
+
             for item in response.get("items", []):
                 if item["id"]["kind"] == "youtube#video":
                     video_id = item["id"]["videoId"]
@@ -81,7 +76,7 @@ def fetch_video_data(start_date, end_date):
 
         with open(cache_filename, 'w') as file:
             json.dump([vars(video) for video in videos], file)
-        
+
         with open(cache_filename, 'r') as file:
             return json.load(file)
 
@@ -95,26 +90,23 @@ def filter_transcript(text):
     try:
         client = OpenAI()
 
-        # Using GPT-3.5 to generate the list of coins from the youtube video transcript
         completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": """You are analyzing a cryptocurrency youtube content creator.
-            Your goal is to identify all the crypto coins or protocols that youtube content creator 
-            regards as profitable or 'bullish'. Please put all of these crypto coin's or protocol's 
-            names in a single python list format, i.e. all names in single quotes inside square brackets, 
-            separated by comma and no other text"""},
-            {"role": "user", "content": text}
-        ]
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": """You are analyzing a cryptocurrency youtube content creator.
+                Your goal is to identify all the crypto coins or protocols that youtube content creator 
+                regards as profitable or 'bullish'. Please put all of these crypto coin's or protocol's 
+                names in a single python list format, i.e. all names in single quotes inside square brackets, 
+                separated by comma and no other text"""},
+                {"role": "user", "content": text}
+            ]
         )
         return completion.choices[0].message.content
     except Exception as e:
         print(f"Failed to filter transcript: {e}")
 
-# if __name__ == "__main__":
-#     fetch_video_data()
 
-start_date = '2024,1,1'
-end_date = '2024,12,31'
-print(fetch_video_data(start_date, end_date))
-
+if __name__ == "__main__":
+    start_date = '2024,3,1'
+    end_date = '2024,12,31'
+    print(fetch_video_data(start_date, end_date))
