ae6d611ae183fe0a7c6ef944a470d8ee19a68967
Miguel <michail.pliuscik@gmail.com>
2024-09-11 20:16:36 +0100

    update

--- transcript.py
+++ transcript.py
@@ -107,6 +107,6 @@ def filter_transcript(text):
 
 # Example usage
 if __name__ == "__main__":
-    start_date = '2024,1,1'
-    end_date = '2024,12,31'
+    start_date = '2024,4,1'
+    end_date = '2024,5,31'
     print(fetch_video_data(start_date, end_date))
